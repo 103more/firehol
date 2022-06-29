b686e4e282f13528f6d92b7dd60d3dd134abf839
103more <108380696+103more@users.noreply.github.com>
2022-06-29 18:26:27 +0900

    Create add_ipaddr.sh

---
+++ add_ipaddr.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+ipaddrs=()
+FILE=`mktemp`
+CODE=`mktemp`
+LIST=`mktemp`
+
+cat > ${FILE} <<EOF1
+127.0.0.1
+XXX.XXX.XXX.XXX
+EOF1
+
+cat > ${CODE} <<EOF2
+400
+401
+402
+403
+404
+EOF2
+
+grep -v -f ${FILE} /var/log/apache2/access.log | grep -f ${CODE} | cut -d' ' -f1 | sort | uniq>${LIST}
+
+while IFS= read -r ipaddr
+do
+        ipaddrs+=("${ipaddr}")
+done < ${LIST}
+
+for ipaddr in ${ipaddrs[@]}
+do
+        echo ""
+        echo ""
+        echo ""
+        echo ""
+        echo "-=-=-=-=-=-  Access Log of ${ipaddr} -=-=-=-=-=-"
+        grep ${ipaddr} /var/log/apache2/access.log
+        read -n1 -p "Do you add the ip addr( ${ipaddr} ) into blocklist_original? (y/N): " yn
+        case "${yn}" in
+                [yY]) ipset a blocklist_original ${ipaddr};;
+        esac
+done
+
+rm -f ${FILE} ${CODE} ${LIST}
